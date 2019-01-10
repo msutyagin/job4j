a2c110fed5cd4a89e0a1a2e51e5f20799a258d82
msutyagin <mori@list.ru>
2019-01-10 16:57:34 +0300

    1.3. JavaDoc. Code convention[#103732]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,17 @@
+
 package ru.job4j.calculate; 
+
+/**
+Класс Калькулятор. Он скоро будет калькулировать.
+@author Maxim Sutyagin (m.sutyagin.m@gmail.com)
+*/
 
 public class Calculate {
 	
+	/**
+	Main
+	@param args - args.
+	*/	
 	public static void main(String[] args){
 		System.out.println("Hello World");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Maxim Sutyagin (m.sutyagin.m@gmail.com)
+* @version 1.0
+* @since 10.01.19
+*/
+package ru.job4j.calculate;
