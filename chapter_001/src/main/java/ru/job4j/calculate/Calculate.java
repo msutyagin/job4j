a247448ec294037807cc50597f62fb3fb5e72370
msutyagin <mori@list.ru>
2019-01-11 13:29:31 +0300

    Code Autoformat ctrl+alt+i

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,26 +1,26 @@
 
-package ru.job4j.calculate; 
+package ru.job4j.calculate;
 
 /**
-Класс Калькулятор. Он скоро будет калькулировать.
-@author Maxim Sutyagin (m.sutyagin.m@gmail.com)
-*/
+ Класс Калькулятор. Он скоро будет калькулировать.
+ @author Maxim Sutyagin (m.sutyagin.m@gmail.com)
+ */
 
 public class Calculate {
-	
+
 	/**
-	Main
-	@param args - args.
-	*/	
+	 Main
+	 @param args - args.
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
-	
+
 	/**
-	* Method echo.
-	* @param name Your name.
-	* @return Echo plus your name.
-	*/
+	 * Method echo.
+	 * @param name Your name.
+	 * @return Echo plus your name.
+	 */
 	public String echo(String name) {
 		return "Echo, echo, echo : " + name;
 	}
