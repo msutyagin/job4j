87e62af898572eadf44e7e4970194eace268aff8
msutyagin <mori@list.ru>
2019-02-21 10:06:46 +0300

    Каркас шахматной доски[#103798] доработка по замечаниям

--- chess/src/test/java/ru/job4j/chess/TestLogic.java
+++ chess/src/test/java/ru/job4j/chess/TestLogic.java
@@ -26,23 +26,22 @@ public class TestLogic {
     /**
      * Тест на метод move. Не по диагонали.
      */
-    @Test
-    public void whenBlackBishopMoveTwoCellsNotDiagonalThenException() {
+    @Test(expected = ImpossibleMoveException.class)
+    public void whenBlackBishopMoveTwoCellsNotDiagonalThenImpossibleMoveException() {
         Logic board = new Logic();
         board.add(new BishopBlack(Cell.C8));
-        boolean rst = board.move(Cell.C8, Cell.E5);
-        assertThat(rst, is(false));
+       board.move(Cell.C8, Cell.E5);
+       // assertThat(rst, is(false));
     }
 
     /**
      * Тест на метод move. По диагонали на пути фигура.
      */
-    @Test
+    @Test(expected = OccupiedWayException.class)
     public void whenBlackBishopMoveTwoCellsDiagonalButOnOneOfThemAnotherFigureStayThenException() {
         Logic board = new Logic();
         board.add(new BishopBlack(Cell.C8));
         board.add(new PawnBlack(Cell.D7));
-        boolean rst = board.move(Cell.C8, Cell.E6);
-        assertThat(rst, is(false));
+        board.move(Cell.C8, Cell.E6);
     }
 }
