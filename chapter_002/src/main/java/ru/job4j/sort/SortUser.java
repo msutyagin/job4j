cf2dfdcf1744c0676adea5519e931674f3d0d6f4
msutyagin <mori@list.ru>
2019-04-04 17:51:50 +0300

    1. Организовать сортировку User[#103747]

--- chapter_002/src/main/java/ru/job4j/sort/SortUser.java
+++ chapter_002/src/main/java/ru/job4j/sort/SortUser.java
@@ -1,10 +1,12 @@
 package ru.job4j.sort;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class SortUser {
+
 
     static class User implements Comparable<User> {
         private String name;
@@ -15,12 +17,35 @@ public class SortUser {
             this.age = age;
         }
 
+
         public String getName(){
             return this.name;
         }
 
         public String getAge() {
             return this.age;
+        }
+
+        @Override
+        public String toString() {
+            return "User{" +
+                    "name'" + name + '\'' +
+                    ", age='" + age + '\'' +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            User user = (User) o;
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            return Objects.equals(name, user.name) &&
+                    Objects.equals(age, user.age);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name, age);
         }
 
         @Override
--- chapter_002/src/test/java/ru/job4j/sort/SortUserTest.java
+++ chapter_002/src/test/java/ru/job4j/sort/SortUserTest.java
@@ -1,9 +1,7 @@
 package ru.job4j.sort;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
+
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -13,7 +11,7 @@ import java.util.List;
 public class SortUserTest {
 
     @Test
-    public void whenInputListOfUsersThenReturnSetOfUsersSortByname() {
+    public void whenInputListOfUsersThenReturnSetOfUsersSortByName() {
         SortUser obj = new SortUser();
         SortUser.User usr1 = new SortUser.User("Vasya", "18");
         SortUser.User usr2 = new SortUser.User("Max", "37");
@@ -23,9 +21,7 @@ public class SortUserTest {
         input.add(usr2);
         input.add(usr3);
         Set<SortUser.User> result = obj.sort(input);
-        for (SortUser.User el : result) {
-            System.out.println(el.getName() + " " + el.getAge());
-        }
-//        assertThat();
+        List<SortUser.User> ex = Arrays.asList(usr1, usr3, usr2);
+        assertThat(result.toString(), is(ex.toString()));
     }
 }
